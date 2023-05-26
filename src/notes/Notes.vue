11f0a6ccf03cf2e39fb639ca9d31732ca2b352c4
Vee-keme <veliaprecious@gmail.com>
2023-05-26 02:26:43 +0100

    notes update

--- src/notes/Notes.vue
+++ src/notes/Notes.vue
@@ -1,3 +1,22 @@
 <template>
-  <h2>this is notes</h2>
+  <main class="h-screen w-screen">
+    <div class="max-w-[1000px] p-[10px] my-0 mx-auto">
+      <header class="flex justify-between items-center">
+        <h1 class="font-bold mb-[25px] text-5xl">Notes</h1>
+        <button
+          class="border-none p-[10px] w-[50px] h-[50px] cursor-pointer bg-slate-900 rounded-full text-white"
+        >
+          +
+        </button>
+      </header>
+      <div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Accusantium, quibusdam earum maiores illo sunt soluta?
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
