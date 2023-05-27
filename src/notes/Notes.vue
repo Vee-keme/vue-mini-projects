1cb66e07978b08c81dcd466304e31d0f331a56b4
Vee-keme <veliaprecious@gmail.com>
2023-05-27 01:57:03 +0100

    add note obj on btn click

--- src/notes/Notes.vue
+++ src/notes/Notes.vue
@@ -1,12 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+const newNote = ref("");
+const notes = ref([]);
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+  newNote.value = "";
+  showModal.value = false;
+};
+</script>
+
 <template>
   <main class="h-screen w-screen">
     <div
+      v-if="showModal"
       class="absolute w-full h-full bg-slate-950/75 z-10 flex items-center justify-center"
     >
       <div
         class="w-[750px] bg-white rounded-lg p-[30px] relative flex flex-col"
       >
         <textarea
+          v-model="newNote"
           class="outline outline-2 outline-slate-400"
           name="note"
           id="note"
@@ -15,12 +40,14 @@
           placeholder="Enter your note here..."
         ></textarea>
         <button
+          @click="addNote"
           class="py-[10px] px-[20px] text-3xl w-[100%] bg-blue-700 border-none text-white cursor-pointer mt-[15px] rounded"
         >
           Add Note
         </button>
 
         <button
+          @click="showModal = false"
           class="py-[10px] px-[20px] text-3xl w-[100%] bg-red-700 border-none text-white cursor-pointer mt-[15px] rounded"
         >
           Close
@@ -31,6 +58,7 @@
       <header class="flex justify-between items-center">
         <h1 class="font-bold mb-[25px] text-5xl">Notes</h1>
         <button
+          @click="showModal = true"
           class="flex items-center justify-center border-none p-[10px] w-[50px] h-[50px] cursor-pointer bg-slate-900 rounded-full text-white text-3xl"
         >
           +
